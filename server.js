381c219d2e6025e7bdc19015bc11595822d97e00
Glitch (rest-api) <none>
2019-12-07 02:03:05 +0000

    👂🏄 Checkpoint ./server.js:7793506/39

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ var server = app.listen(3000, function () {
     });
   
    app.get("/getAllOrders", function(req, res) {
-      return collectionOrders.find({},  {projection: {  _id: 0,orderNo: 1 }}).toArray(function(error, response) {
+      return collectionOrders.find({}).toArray(function(error, response) {
         console.log("response", response)
         return res.send(response)
     });
