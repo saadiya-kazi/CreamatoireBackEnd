5842c2e87498a521f5d6b1d590b95479dc7733ad
Glitch (skinny-redcurrant) <none>
2019-12-07 04:37:06 +0000

    🌒💳 Checkpoint ./server.js:7793506/4958 ./package.json:7793506/855

--- server.js
+++ server.js
@@ -6,10 +6,9 @@
 // port to listen to for requests. In this case, port 3000.
 // 
 var express = require("express");
-var socketIo = require("socket.io");
 var app = express();
 var http = require("http").createServer(app);
-// const io = socketIo(http);
+const io = require("socket.io")(http);
 var bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
@@ -24,98 +23,99 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
  
 // var routes = require("./routes.js")(app);
- 
+//     app.get("/getIngredientsList", function(req, res) {
+//       return collection.find({}).toArray(function(error, response) {
+//         console.log("response", response)
+//         return res.send(response)
+//     });
+//     });
+//    app.get("/getOrderBeingPrepared", function(req, res) {
+//       return collectionOrders.find({status: "being_prepared"},  {projection: {  _id: 0,orderNo: 1 }}).toArray(function(error, response) {
+//         console.log("response", response)
+//         return res.send(response)
+//     });
+//     });
+  
+//   app.get("/getOrderReadyForPickup", function(req, res) {
+//       return collectionOrders.find({status: "ready_for_pickup"},  {projection: {  _id: 0,orderNo: 1 }}).toArray(function(error, response) {
+//         console.log("response", response)
+//         return res.send(response)
+//     });
+//     });
+//    app.get("/getAllOrders", function(req, res) {
+//       return collectionOrders.find({}).toArray(function(error, response) {
+//         console.log("response", response)
+//         return res.send(response)
+//     });
+//     });
+//   app.put('/updateStatus', function(req, res) {
+//     const query = {
+//       orderNo:  req.body.orderNo
+//     }
+//     var newStatus = { $set: { status: req.body.status } }
+//     return collectionOrders.updateOne(query, newStatus, function(err, response) {
+//       return res.send({success: true, message: "Updated Successfully", data: req.body})
+//     })
+
+//   })
+//   app.post("/createOrder", function(req, res) {
+//      console.log("reqBody", req.body)
+//      const object = {
+//        status: "being_prepared",
+//      }
+//      object['ingredientList'] = req.body.ingredientList
+//      console.log("object", object)
+//       return collectionOrders.find({}).toArray(function(error, response) {
+//         console.log("response", response)
+//         if(response.length === 0) {
+//           object['orderNo'] = 1
+//           return collectionOrders.insertOne(object, function(error, responseSave) {
+//             return res.send({ success: true, message:"Data Saved Successfully", data:object })
+//           })
+//         } else {
+//           object['orderNo'] = response.length +1;
+//            return collectionOrders.insertOne(object, function(error, responseSave) {
+//             return res.send({ success: true, message:"Data Saved Successfully", data:object })
+//           })
+//         }
+        
+//     });
+    
+//    });
+
 
 http.listen(port, function () {
   console.log("Listening on port %s", port);
-   MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        database = client.db(DATABASE_NAME);
-        collection = database.collection("TESTCOLL");
-       collectionOrders = database.collection("TESTORDERS")
+//    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+//         if(error) {
+//             throw error;
+//         }
+//         database = client.db(DATABASE_NAME);
+//         collection = database.collection("TESTCOLL");
+//        collectionOrders = database.collection("TESTORDERS")
      
-        console.log("Connected to `" + DATABASE_NAME + "`!");
-    });
-    app.get("/getIngredientsList", function(req, res) {
-      return collection.find({}).toArray(function(error, response) {
-        console.log("response", response)
-        return res.send(response)
-    });
-    });
-   app.get("/getOrderBeingPrepared", function(req, res) {
-      return collectionOrders.find({status: "being_prepared"},  {projection: {  _id: 0,orderNo: 1 }}).toArray(function(error, response) {
-        console.log("response", response)
-        return res.send(response)
-    });
-    });
-  
-  app.get("/getOrderReadyForPickup", function(req, res) {
-      return collectionOrders.find({status: "ready_for_pickup"},  {projection: {  _id: 0,orderNo: 1 }}).toArray(function(error, response) {
-        console.log("response", response)
-        return res.send(response)
-    });
-    });
-   app.get("/getAllOrders", function(req, res) {
-      return collectionOrders.find({}).toArray(function(error, response) {
-        console.log("response", response)
-        return res.send(response)
-    });
-    });
-  app.put('/updateStatus', function(req, res) {
-    const query = {
-      orderNo:  req.body.orderNo
-    }
-    var newStatus = { $set: { status: req.body.status } }
-    return collectionOrders.updateOne(query, newStatus, function(err, response) {
-      return res.send({success: true, message: "Updated Successfully", data: req.body})
-    })
-
-  })
-  app.post("/createOrder", function(req, res) {
-     console.log("reqBody", req.body)
-     const object = {
-       status: "being_prepared",
-     }
-     object['ingredientList'] = req.body.ingredientList
-     console.log("object", object)
-      return collectionOrders.find({}).toArray(function(error, response) {
-        console.log("response", response)
-        if(response.length === 0) {
-          object['orderNo'] = 1
-          return collectionOrders.insertOne(object, function(error, responseSave) {
-            return res.send({ success: true, message:"Data Saved Successfully", data:object })
-          })
-        } else {
-          object['orderNo'] = response.length +1;
-           return collectionOrders.insertOne(object, function(error, responseSave) {
-            return res.send({ success: true, message:"Data Saved Successfully", data:object })
-          })
-        }
-        
-    });
-    
-   });
+//         console.log("Connected to `" + DATABASE_NAME + "`!");
+//     });
+ 
 });
 
 
 
-// io.on("connection", function(socket) {
-//   console.log("user connected");
-//   // socket.emit("FromAPI", "hi");
-//   socket.on("subscribe", function(room) {
-//     console.log("joining room", room);
-//     socket.join(room);
-//   });
-//   socket.on("unsubscribe", function(room) {
-//     console.log("left room", room);
-//     socket.leave(room);
-//   });
-//   socket.on("disconnect", function() {
-//     console.log("user disconnected");
-//   });
-// });
+io.on("connection", function(socket) {
+  console.log("user connected");
+  // socket.emit("FromAPI", "hi");
+  socket.on("subscribe", function(room) {
+    console.log("joining room", room);
+    socket.join(room);
+  });
+  socket.on("unsubscribe", function(room) {
+    console.log("left room", room);
+    socket.leave(room);
+  });
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+});
 
 
 
